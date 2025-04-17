df07f49314cfbf68265c4ccc44bfd5174b11feb1
Jasec <jasec_7@hotmail.com>
2025-04-17 02:44:30 -0700

    complete react hooks lists and keys lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,15 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+  const projectArr = projects.map((proObj)=>{
+    return <ProjectItem key= {proObj.id} name={proObj.name} about={proObj.about} technologies={proObj.technologies} />
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectArr}</div>
     </div>
   );
 }
